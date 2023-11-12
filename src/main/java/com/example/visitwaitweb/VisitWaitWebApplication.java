6a7c725653048642348af374fe206e7935c16c42
anstjaos <vv_eros@naver.com>
2023-11-12 21:07:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/visitwaitweb/VisitWaitWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.visitwaitweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitWaitWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitWaitWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/visitwaitweb/VisitWaitWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visitwaitweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitWaitWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
